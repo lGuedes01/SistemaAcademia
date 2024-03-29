3fc03c73d36672fccc6c0304e99ea8e5b309d2c4
Lucas Guedes <lucasguedescorreia01@gmail.com>
2024-03-29 15:56:53 -0300

    First commit

---
+++ src/Aluno.java
@@ -0,0 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Aluno {
+    protected Date dataNascimento;
+    protected String CPF;
+    protected String name;
+
+
+}
---
+++ src/Planos.java
@@ -0,0 +1,5 @@
+public class Planos {
+    protected int codigo;
+    protected String name;
+    protected float preco;
+}
